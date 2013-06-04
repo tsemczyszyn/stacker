19a98f7bb12b9fd6c3323a59150ab89cc67de2e3
Travis Semczyszyn <tsemczyszyn@sangoma.com>
2013-06-04 14:49:17 -0400

    Task creating now works

--- stacker.py
+++ stacker.py
@@ -3,21 +3,23 @@
 import time
 import curses
 import hashlib
+import sys
 
 class Task:
 
     def __init__(self):
-        title = "default"
-        taskID = hashlib.md5()
-        creation = time.time() 
-        elapsed = 0
-        active = False
-        selected = False
+            
+        self.title = "default"
+        self.taskID = hashlib.md5()
+        self.creation = time.time() 
+        self.elapsed = 0
+        self.active = False
+        self.selected = False
 
-        timer_start = 0
-        timer_stop = 0
+        self.timer_start = 0
+        self.timer_stop = 0
 
-        taskID.update(creation)
+        self.taskID.update(str(self.creation))
 
     def activate():
         self.timer_start = time.time()
@@ -29,6 +31,9 @@ class Task:
         self.active = False
 
 class Stacker:
+
+    stack = []
+
 
     #Create the window for curses stuff
     mainscr = curses.initscr()
@@ -65,10 +70,24 @@ class Stacker:
     description.addstr(0, 1, "Description")
     description.refresh()
     curses.curs_set(0)
-    mainscr.getch()
+    curses.noecho()
 
     #Need a main loop here
     #Loop redraw the screen and check for input
 
-    curses.endwin()
+    while (1):
+        key = mainscr.getkey()
+        if (key == 'n'):
+            newTask = Task()
+            stack.append(newTask)
+        elif (key == 'x'):
+            curses.endwin()
+            sys.exit(0)
+        key = ''
+        
+        taskwin.move(1, 1)
+        for position, task in enumerate(stack):
+            taskwin.addstr(task.title + "\t" + str(task.taskID.hexdigest()))
+            taskwin.move(position+2, 1)
 
+        taskwin.refresh()
