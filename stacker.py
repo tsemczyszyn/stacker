c758275b2baf8f9cbdce3643a3ac1b6c9f5f8a6f
Travis Semczyszyn <tsemczyszyn@gmail.com>
2013-06-05 22:21:12 -0400

    Added the rendering thread

--- stacker.py
+++ stacker.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
 import time
+import datetime
 import curses
 import hashlib
 import sys
-
-
-stack = []
+import thread
 
 class Task:
 
@@ -30,92 +29,108 @@ class Task:
 
     def deactivate(self):
         self.timer_stop = time.time()
-        self.elapsed = self.timer_stop - self.timer_start
+        self.elapsed += self.timer_stop - self.timer_start
         self.active = False
 
-class Stacker:
 
-    #Create the window for curses stuff
-    mainscr = curses.initscr()
+stack = []
+render = True
 
-    #Menu
-    mainscr.addstr("\n\n\n N", curses.A_UNDERLINE)
-    mainscr.addstr("ew Task\n")
-    mainscr.addstr(" E", curses.A_UNDERLINE)
-    mainscr.addstr("dit Task\n")
-    mainscr.addstr(" C", curses.A_UNDERLINE)
-    mainscr.addstr("lose Task\n")
-    mainscr.addstr(" C")
-    mainscr.addstr("a", curses.A_UNDERLINE)
-    mainscr.addstr("ncel Task\n")
-    mainscr.addstr(" E")
-    mainscr.addstr("x", curses.A_UNDERLINE)
-    mainscr.addstr("it\n")
+#Create the window for curses stuff
+mainscr = curses.initscr()
 
-    mainscr.border(0)
-    mainscr.addstr(0, 1, "Task Stacker")
-    mainscr.refresh()
+#Menu
+mainscr.addstr("\n\n\n N", curses.A_UNDERLINE)
+mainscr.addstr("ew Task\n")
+mainscr.addstr(" E", curses.A_UNDERLINE)
+mainscr.addstr("dit Task\n")
+mainscr.addstr(" C", curses.A_UNDERLINE)
+mainscr.addstr("lose Task\n")
+mainscr.addstr(" C")
+mainscr.addstr("a", curses.A_UNDERLINE)
+mainscr.addstr("ncel Task\n")
+mainscr.addstr(" E")
+mainscr.addstr("x", curses.A_UNDERLINE)
+mainscr.addstr("it\n")
 
-    dims = mainscr.getmaxyx()
-    
-    #Initialize Task Window
-    taskwin = mainscr.subwin(dims[0]-4, (dims[1]/2)-7, 2, 15)
-    taskwin.border(0)
-    taskwin.addstr(0, 1, "Tasks")
-    taskwin.refresh()
-    taskdims = taskwin.getmaxyx()
+mainscr.border(0)
+mainscr.addstr(0, 1, "Task Stacker")
+mainscr.refresh()
 
-    #Stackwin
-    stackwin = taskwin.derwin(taskdims[0]-2, taskdims[1]-2, 1, 1)
+dims = mainscr.getmaxyx()
 
-    #Initialize Description Window
-    description = mainscr.subwin(dims[0]-4, (dims[1]/2)-10, 2, (dims[1]/2)+9)
-    description.border(0)
-    description.addstr(0, 1, "Description")
-    description.refresh()
-    curses.curs_set(0)
-    curses.noecho()
+#Initialize Task Window
+taskwin = mainscr.subwin(dims[0]-4, (dims[1]/2)-7, 2, 15)
+taskwin.border(0)
+taskwin.addstr(0, 1, "Tasks")
+taskwin.refresh()
+taskdims = taskwin.getmaxyx()
 
-    def addTask():
+#Stackwin
+stackwin = taskwin.derwin(taskdims[0]-2, taskdims[1]-2, 1, 1)
 
-        global stack
-        newTask = Task()
-        newTask.activate()
-        if (len(stack) != 0):
-            stack[len(stack)-1].deactivate()
+#Initialize Description Window
+description = mainscr.subwin(dims[0]-4, (dims[1]/2)-10, 2, (dims[1]/2)+9)
+description.border(0)
+description.addstr(0, 1, "Description")
+description.refresh()
+curses.curs_set(0)
+curses.noecho()
 
-        stack.append(newTask)
 
-    def closeTask():
+def addTask():
 
-        global stack
-        if (len(stack) != 0):
-            stack[len(stack)-2].activate()
-            stack.pop()
+    newTask = Task()
+    newTask.activate()
+    if (len(stack) != 0):
+        stack[len(stack)-1].deactivate()
 
-    #Need a main loop here
-    #Loop redraw the screen and check for input
+    stack.append(newTask)
 
-    while (1):
+def closeTask():
 
-        key = mainscr.getkey()
+    if (len(stack) != 0):
+        stack[len(stack)-2].activate()
+        stack.pop()
 
-        if (key == 'n'):
-            addTask()
+def redrawStack():
 
-        elif (key == 'c'):
-            closeTask()
+    stackwin.move(0,0)
 
-        elif (key == 'x'):
-            curses.endwin()
-            sys.exit(0)
+    for position, task in enumerate(stack):
 
-        key = ''
-        stackwin.move(0,0)
-        for position, task in enumerate(stack):
-            stackwin.addstr(task.title + "\t" + str(task.taskID.hexdigest()) + "\t" + str(task.active))
-            stackwin.clrtoeol()
-            stackwin.move(position+1, 0)
+        if (task.active == True):
+            stackwin.addstr("\t" + task.title + "\t" + str(task.taskID.hexdigest()) + "\t" + str(datetime.timedelta(seconds=task.elapsed+(time.time() - task.timer_start))))
+        else:
+            stackwin.addstr(task.title + "\t" + str(task.taskID.hexdigest()) + "\t" + str(task.elapsed))
 
-        stackwin.clrtobot()
-        stackwin.refresh()
+        stackwin.clrtoeol()
+        stackwin.move(position+1, 0)
+
+    stackwin.clrtobot()
+    stackwin.refresh()
+
+def threadedRedraw():
+    while (render):
+        redrawStack()
+        time.sleep(0.25)
+
+thread.start_new_thread(threadedRedraw, ())
+
+while (1):
+
+    key = mainscr.getkey()
+
+    if (key == 'n'):
+        addTask()
+        redrawStack()
+
+    elif (key == 'c'):
+        closeTask()
+        redrawStack()
+
+    elif (key == 'x'):
+        render = False
+        curses.endwin()
+        sys.exit(0)
+
